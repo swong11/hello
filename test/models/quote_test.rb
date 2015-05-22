e56f5cb2053339bef0487d3f5eeb12a3d53d7ad4
swong11 <sunwong11@gmail.com>
2015-05-22 05:45:24 +0000

    Display ID number plus initial of author

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+ 
+	test "unique_tag" do 
+		quote = Quote.create(:author => 'Walt Disney', :saying => 'If you can dream it, you can do it.')
+		
+		expected = 'WD#' + quote.id.to_s
+		actual = quote.unique_tag
+		assert_equal expected, actual
+	end
+
 end
